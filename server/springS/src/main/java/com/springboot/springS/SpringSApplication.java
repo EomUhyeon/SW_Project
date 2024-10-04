8d0d41c514f44f9c3146267d7d17a58530aa221d
m2m0ryc1oud <m2m0ryc1oud@gmail.com>
2024-10-04 14:03:13 +0900

    test

---
+++ server/springS/src/main/java/com/springboot/springS/SpringSApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSApplication.class, args);
+	}
+
+}
---
+++ server/springS/src/test/java/com/springboot/springS/SpringSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
